5d517d630eac879b221aa842960af5eb961b9e60
Bruno Silva_CONTR <c0603070@vale.com>
2020-05-05 17:00:25 -0300

    Adicionando rota para criar usuário

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,3 +4,11 @@ const { userData } = require('../data');
 module.exports.getUsers = (req, res) => {
     res.status(200).json(userData);
 };
+
+module.exports.create = (req, res) => {
+    let data = req.body;
+    const newUser = new User(data.name, data.age, data.email);
+
+    userData.push(newUser);
+    res.status(201).json(newUser);
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/user.controller');
 const router = new Router();
 
 router.get('/users', userController.getUsers);
+router.post('/users', userController.create);
+
 
 module.exports = router;
