b85114c8078f35831cf7db1d261d016b98e707e8
Bruno Silva_CONTR <c0603070@vale.com>
2020-05-05 17:05:04 -0300

    Importando rotas no app e disponibilizando na API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+const userRouter = require('./routes/user.routes');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(userRouter);
 
 module.exports = app;
