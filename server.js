e2907cc4621e20b0da915c758050c4a1942e9915
Bruno Silva_CONTR <c0603070@vale.com>
2020-05-05 16:53:50 -0300

    Modularizando app para um arquivo separado

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const server = require('./src/app');
 
 const port = process.env.PORT | 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
